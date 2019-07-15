fd875474460101c2da6762933c543afd5e936ef8
cheungkinwong <cheungkin.wong@gmail.com>
2019-07-15 17:01:16 +0200

    get pokemon-species

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-//declare variables
 const input = document.getElementById("input");
 const sprites = document.getElementById("sprites");
+const familySprites = document.getElementById("familySprites");
 const stats = document.getElementById("stats");
 const number = document.getElementById("number");
 const name = document.getElementById("name");
@@ -8,27 +8,36 @@ const moves = document.getElementById("moves");
 const type = document.getElementById("type");
 
 input.addEventListener("keyup", key => {
-    let pokemon = input.value.toLowerCase();
-    if (key.keyCode == 13) {
-        getPokedex(pokemon);
-    }
+     let pokemon = input.value.toLowerCase();
+     if (key.keyCode == 13) {
+          getPokedex(pokemon);
+     }
 });
 
 async function getPokedex(input) {
-    let request = `https://pokeapi.co/api/v2/pokemon/${input}`;
-    let response = await fetch(request);
-    let pokeEntry = await response.json();
-    await updatePokedex(pokeEntry);
+     let request = `https://pokeapi.co/api/v2/pokemon/${input}`;
+     let requestSpecies = `https://pokeapi.co/api/v2/pokemon-species/${input}`;
+     let response = await fetch(request, { mode: "cors" });
+     let responseSpecies = await fetch(requestSpecies, { mode: "cors" });
+     let pokeEntry = await response.json();
+     let speciesEntry = await responseSpecies.json();
+     console.log(speciesEntry);
+     await updatePokedex(pokeEntry);
 }
 
 function updatePokedex(pokeEntry) {
-    sprites.src = pokeEntry.sprites.front_default;
-    name.innerHTML = "n°" + pokeEntry.id + " " + pokeEntry.species.name;
-    //  type.innerHTML = pokeEntry.types
-    moves.innerHTML =
-        pokeEntry.moves[0].move.name + "<br>" + pokeEntry.moves[1].move.name + "<br>" + pokeEntry.moves[2].move.name + "<br>" + pokeEntry.moves[3].move.name;
-}
-// next button function
-function nextPokemonButton() {}
+     console.log(pokeEntry);
 
-console.log('lol');
+     sprites.src = pokeEntry.sprites.front_default;
+     number.innerHTML = "n°" + pokeEntry.id;
+     name.innerHTML = pokeEntry.species.name;
+
+     type.innerHTML = " ";
+     pokeEntry.types.forEach(name => {
+          console.log(name.type.name);
+          type.innerHTML += name.type.name + " ";
+     });
+
+     moves.innerHTML =
+          pokeEntry.moves[0].move.name + "<br>" + pokeEntry.moves[1].move.name + "<br>" + pokeEntry.moves[2].move.name + "<br>" + pokeEntry.moves[3].move.name;
+}
--- readme.md
+++ readme.md
@@ -1,5 +1,1 @@
 #Pokedex
-
-zukisa
-
-
